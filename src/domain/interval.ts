eedb576c64790e3d32af3801833d07a240781714
Maykon Oliveira <maykonoliveira850@gmail.com>
2023-03-25 17:26:21 +0000

    feat: notes calculator

--- src/domain/interval.ts
+++ src/domain/interval.ts
@@ -1,13 +1,23 @@
-type Numbers = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-export type Ts = `${Numbers}T`;
-export type Interval = Ts | `${Ts}H`;
+import { NoteType } from "./note-type";
 
-export function distance(interval: Interval): number {
-    let num = Number(interval.charAt(0)) * 2
-    
+type Numbers = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+export type Ts = `${Numbers}T`;
+export type Interval = Ts | `${Ts}H` | 'TH';
+
+export function decimalDistance(interval: Interval): number {
+    const groups = interval.match(/[0-9]+/);
+    let num = (groups ? Number(groups[0]) : 0) * 2;
+
     if (interval.endsWith('H')) {
         num++;
     }
 
     return num;
+}
+
+export function increaseInterval(firstNote: NoteType, interval: Interval) {
+    const notes = Object.values(NoteType);
+    const thisIndex = notes.indexOf(firstNote);
+    const nextNoteIndex = thisIndex + decimalDistance(interval);
+    return notes[nextNoteIndex % notes.length];
 }
--- test/domain/interval.test.ts
+++ test/domain/interval.test.ts
@@ -1,11 +1,22 @@
-import { distance } from "../../src/domain/interval";
+import { increaseInterval, decimalDistance } from "../../src/domain/interval";
+import { NoteType } from "../../src/domain/note-type";
 
 describe('Interval suitcase', () => {
-  it('distance', () => {
-    expect(distance('2T')).toEqual(4);
-    expect(distance('2TH')).toEqual(5);
-    expect(distance('1T')).toEqual(2);
-    expect(distance('9T')).toEqual(18);
-    expect(distance('9TH')).toEqual(19);
+  it('decimalDistance', () => {
+    expect(decimalDistance('2T')).toEqual(4);
+    expect(decimalDistance('2TH')).toEqual(5);
+    expect(decimalDistance('1T')).toEqual(2);
+    expect(decimalDistance('9T')).toEqual(18);
+    expect(decimalDistance('9TH')).toEqual(19);
+  });
+
+  it('increaseInterval', () => {
+    expect(increaseInterval(NoteType.C, '1T')).toEqual(NoteType.D);
+    expect(increaseInterval(NoteType.D$, '1T')).toEqual(NoteType.F);
+    expect(increaseInterval(NoteType.B, '1T')).toEqual(NoteType.C$);
+    expect(increaseInterval(NoteType.G, 'TH')).toEqual(NoteType.G$);
+    expect(increaseInterval(NoteType.F, '4TH')).toEqual(NoteType.D);
+    expect(increaseInterval(NoteType.C, '12T')).toEqual(NoteType.C);
+    expect(increaseInterval(NoteType.C, '12TH')).toEqual(NoteType.C$);
   });
 });
